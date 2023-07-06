c0de568a831761a3c50e546cf5978f9302932278
bitilitos <mailparaonuno@gmail.com>
2023-07-06 16:00:01 +0100

    Done

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,42 @@ use std::ops::Add;
 use std::io;
 
 fn main() {
+    println!("\n*****************************************");
+    println!("** FIBONACCI SEQUENCE VALUE CALCULATOR **");
+    println!("*****************************************\n");
 
-    println!("Enter a number for the index of the fibonacci sequence.");
+    'main: loop{
+        let index:u8 ='input: loop{
+            let input= get_input();
+            if is_quit(input.trim()) {
+                break 'main;
+            }
+            let index = get_index(input.as_str());
+            if !is_index_within_limits(index.clone()) {
+                println!("Invalid input!!!");
+                continue 'input;
+            }else {
+                break 'input index;
+            }
+        };
+        println!("The {}{} fibonacci element is {}.\n", index, ordinal_string(index), format_number(return_fib_element(index)));
+    }
+}
 
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Could't read input!");
+fn get_input() -> String {
+        println!("Please enter a number from 0 up to 186, or q to quit.");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Could't read input!");
+        input
+}
 
-    let index:u8 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(num) => 255,
-    };
-
-
+fn get_index(input: &str) -> u8{
+    match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 255,
+    }
 }
 
 fn return_fib_element(index:u8) -> u128 {
@@ -37,3 +59,41 @@ fn calculate_fib_element(index:u8) -> u128 {
     }
     last_fib_numbers[0].add(last_fib_numbers[1])
 }
+
+fn ordinal_string(index:u8) -> String {
+    match index%10 {
+        1 => String::from("st"),
+        2 => String::from("nd"),
+        3 => String::from("rd"),
+        _ => String::from("th"),
+    }
+}
+
+fn format_number(number:u128) -> String {
+    let mut str_number:String = number.to_string();
+    if str_number.len() > 3 {
+        for i in (1..=str_number.len()-3).rev().step_by(3){
+                str_number.insert(i,' ');
+        }
+    }
+    str_number
+}
+
+fn is_index_within_limits(index:u8) -> bool {
+    match index {
+        0..=186 => true,
+        _ => false,
+    }
+}
+
+fn is_quit(input:&str) -> bool {
+    match input{
+        "q" => true,
+        "Q" => true,
+        "quit" => true,
+        "QUIT" => true,
+        "Quit" => true,
+        _ => false,
+    }
+}
+
