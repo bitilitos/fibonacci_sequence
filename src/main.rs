ef75d225de3ed6daa12bd2923320a8f283368172
bitilitos <mailparaonuno@gmail.com>
2023-07-07 16:41:22 +0100

    Elapsed time of calculation. Use of BigDecimal to increase range.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,82 @@
-use std::ops::Add;
+use bigdecimal::BigDecimal;
 use std::io;
+use std::ops::Add;
+use std::time::Instant;
 
 fn main() {
     println!("\n*****************************************");
     println!("** FIBONACCI SEQUENCE VALUE CALCULATOR **");
     println!("*****************************************\n");
 
-    'main: loop{
-        let index:u8 ='input: loop{
-            let input= get_input();
+    'main: loop {
+        let index: u32 = 'input: loop {
+            let input = get_input();
             if is_quit(input.trim()) {
                 break 'main;
             }
             let index = get_index(input.as_str());
-            if !is_index_within_limits(index.clone()) {
+            if let Some(i) = index {
+                break 'input i;
+            } else {
                 println!("Invalid input!!!");
                 continue 'input;
-            }else {
-                break 'input index;
             }
         };
-        println!("The {}{} fibonacci element is {}.\n", index, ordinal_string(index), format_number(return_fib_element(index)));
+        let start = Instant::now();
+        println!(
+            "The {}{} fibonacci element is {}.",
+            index,
+            ordinal_string(index),
+            format_number(return_fib_element(index))
+        );
+        let duration = start.elapsed();
+        println!(
+            "The calculation algorithm of the {}{} fibonacci element took {:?}\n",
+            index,
+            ordinal_string(index),
+            duration
+        );
     }
 }
 
 fn get_input() -> String {
-        println!("Please enter a number from 0 up to 186, or q to quit.");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Could't read input!");
-        input
+    println!("Please enter a number from 0 up to 4294967295, or q to quit.");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Could't read input!");
+    input
 }
 
-fn get_index(input: &str) -> u8{
+fn get_index(input: &str) -> Option<u32> {
     match input.trim().parse() {
-        Ok(num) => num,
-        Err(_) => 255,
+        Ok(num) => Some(num),
+        Err(_) => None,
     }
 }
 
-fn return_fib_element(index:u8) -> u128 {
+fn return_fib_element(index: u32) -> BigDecimal {
     match index {
-        0 => 0,
-        1 => 1,
-        _ => calculate_fib_element(index)
+        0 => BigDecimal::from(0),
+        1 => BigDecimal::from(1),
+        _ => calculate_fib_element(index),
     }
 }
 
-fn calculate_fib_element(index:u8) -> u128 {
-    let mut last_fib_numbers:[u128;2] = [0, 1];
+fn calculate_fib_element(index: u32) -> BigDecimal {
+    let mut last_fib_numbers: [BigDecimal; 2] = [BigDecimal::from(0), BigDecimal::from(1)];
     for i in 2..index {
-        if i%2==0 {
-            last_fib_numbers[0] = last_fib_numbers[0].add(last_fib_numbers[1]);
+        if i % 2 == 0 {
+            last_fib_numbers[0] = last_fib_numbers[0].clone().add(&last_fib_numbers[1]);
         } else {
-            last_fib_numbers[1] = last_fib_numbers[0].add(last_fib_numbers[1]);
+            last_fib_numbers[1] = last_fib_numbers[0].clone().add(&last_fib_numbers[1]);
         }
     }
-    last_fib_numbers[0].add(last_fib_numbers[1])
+    last_fib_numbers[0].clone().add(&last_fib_numbers[1])
 }
 
-fn ordinal_string(index:u8) -> String {
-    match index%10 {
+fn ordinal_string(index: u32) -> String {
+    match index % 10 {
         1 => String::from("st"),
         2 => String::from("nd"),
         3 => String::from("rd"),
@@ -69,25 +84,18 @@ fn ordinal_string(index:u8) -> String {
     }
 }
 
-fn format_number(number:u128) -> String {
-    let mut str_number:String = number.to_string();
+fn format_number(number: BigDecimal) -> String {
+    let mut str_number: String = number.to_string();
     if str_number.len() > 3 {
-        for i in (1..=str_number.len()-3).rev().step_by(3){
-                str_number.insert(i,' ');
+        for i in (1..=str_number.len() - 3).rev().step_by(3) {
+            str_number.insert(i, ' ');
         }
     }
     str_number
 }
 
-fn is_index_within_limits(index:u8) -> bool {
-    match index {
-        0..=186 => true,
-        _ => false,
-    }
-}
-
-fn is_quit(input:&str) -> bool {
-    match input{
+fn is_quit(input: &str) -> bool {
+    match input {
         "q" => true,
         "Q" => true,
         "quit" => true,
@@ -96,4 +104,3 @@ fn is_quit(input:&str) -> bool {
         _ => false,
     }
 }
-
